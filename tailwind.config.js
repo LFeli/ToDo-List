c13d0ab06eaa7e0b15aada7f41c9c6d33fb4468a
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-02 14:30:17 -0300

    :lipstick: style: Added complementary color tones

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ export default {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      colors: {}
+      colors: {
+        'blue': '#4EA8DE',
+        'blueDark': '#1E6F9F',
+
+        'purple': '#8284FA',
+        'purpleDark': '#5E60CE',
+
+        'danger': '#E25858',
+      }
     },
   },
   plugins: [],

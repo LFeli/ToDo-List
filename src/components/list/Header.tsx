765a12a78d50bb13f9ae73ebe152c5ba80267542
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-21 20:50:52 -0300

    :sparkles: feat: Added counts in Header on list components

--- src/components/list/Header.tsx
+++ src/components/list/Header.tsx
@@ -1,14 +1,21 @@
-export function Header() {
+interface HeaderProps {
+  tasksCounter: number
+  checkedTasksCounter: number
+}
+
+export function Header({ tasksCounter, checkedTasksCounter }: HeaderProps) {
   return (
     <header className="flex items-center justify-between">
       <aside className="flex items-center gap-2">
         <p className="text-sm font-bold leading-snug text-blue ">Tarefas criadas</p>
-        <span className="text-xs text-gray-100 py-[2px] px-2 bg-gray-400 rounded-full">0</span>
+        <span className="text-xs text-gray-100 py-[2px] px-2 bg-gray-400 rounded-full">{tasksCounter}</span>
       </aside>
 
       <aside className="flex items-center gap-2">
         <p className="text-sm font-bold leading-snug text-purple">Concluídas</p>
-        <span className="text-xs text-gray-100 py-[2px] px-2 bg-gray-400 rounded-full">0</span>
+        <span className="text-xs text-gray-100 py-[2px] px-2 bg-gray-400 rounded-full">
+          { tasksCounter === 0 ? tasksCounter : `${checkedTasksCounter} de ${tasksCounter}` }
+        </span>
       </aside>
     </header>
   )

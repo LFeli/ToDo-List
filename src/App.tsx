6a467a3754d9c7aa6656e8a41b2ba3c37d79a853
Lucas Felipe <lucasfelipemk@gmail.com>
2024-01-20 23:12:39 -0300

    :recycle: refactor: Added all components in the 'App.tsx' page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import { Check, ClipboardList, PlusCircle, Trash2 } from "lucide-react";
-import logo from './assets/logo.svg';
+import { Header } from "./components/Header";
+import { Header as HeaderList } from "./components/list/Header";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
+import { EmptyTask } from "./components/list/EmptyTasks";
+import { ItensTasks } from "./components/list/ItensTasks";
+import { PlusCircle } from "lucide-react";
 
 export function App() {
-  const [isChecked, setIsChecked] = useState(false);
 
-  function handleToggleCheckButton() {
-    setIsChecked(!isChecked);
-  }
 
   return (
     <body className="bg-gray-600 text-gray-100 h-screen">
@@ -17,87 +17,27 @@ export function App() {
       {/* Container */}
       <div className="max-w-3xl mx-auto -mt-[126px] px-6">
         <main>
-          <img 
-            src={logo} 
-            alt="ToDo - Logo" 
-            className="pb-12 mx-auto"
-          />
+
+          <Header />
 
           <form className="flex gap-2">
             {/* <label htmlFor="newTask">Descrição da tarefa |</label> */}
             
-            <input 
-              type="text"
-              id="newTask"
-              placeholder="Adicione uma nova tarefa" 
-              className="rounded-lg p-4 flex-1 bg-gray-500 placeholder:text-gray-300"
-            />
+            <Input />
 
-            <button className="p-4 text-sm font-bold leading-snug rounded-lg flex items-center gap-2 bg-blueDark hover:bg-blue transition-colors">
+            <Button className="p-4 text-sm font-bold leading-snug rounded-lg flex items-center gap-2 bg-blueDark hover:bg-blue transition-colors">
               Criar
               <PlusCircle size={16} />
-            </button>
+            </Button>
           </form>
 
           <div className="mt-16">
             <div>
-              <div className="flex items-center justify-between">
-                <span 
-                  className="text-sm font-bold leading-snug text-blue flex items-center gap-2">
-                    Tarefas criadas 
-                    <span className="text-xs text-gray-100 py-[2px] px-2 bg-gray-400 rounded-full">
-                      0
-                    </span> 
-                </span>
+              <HeaderList />
 
-                <span 
-                  className="text-sm font-bold leading-snug text-purple flex items-center gap-2">
-                    Concluídas
-                    <span className="text-xs text-gray-100 py-[2px] px-2 bg-gray-400 rounded-full">
-                      0
-                    </span> 
-                </span>
-              </div>
+              <EmptyTask />
 
-              {/* <div className="mt-6 py-16 flex flex-col items-center justify-center gap-4 border-t border-gray-400 rounded-lg">
-                <ClipboardList size={96} className="text-gray-400"/>
-
-                <p className="text-gray-300 text-base text-center leading-snug flex flex-col">
-                  <span className="font-bold">Você ainda não tem tarefas cadastradas</span>
-                  Crie tarefas e organize seus itens a fazer
-                </p>
-              </div> */}
-
-              <div className="mt-6 grid gap-3">
-                <article className="p-4 bg-gray-500 border border-gray-400 rounded-lg flex items-start justify-between gap-3">
-                  <label className="cursor-pointer">
-                    <input
-                      type="checkbox"
-                      checked={isChecked}
-                      onChange={handleToggleCheckButton}
-                      className="hidden"
-                    />
-
-                    <div
-                      className={`w-5 h-5 border-2 rounded-full ${
-                        isChecked ? 'bg-purpleDark border-purpleDark' : 'bg-gray-600 border-blue'
-                      } flex items-center justify-center`}>
-                        
-                      {isChecked && (
-                        <Check size={12}/>
-                      )}
-                    </div>
-                  </label>
-                  
-                  <p className={`text-sm text-left leading-snug max-w-[632px] ${isChecked ? 'line-through text-gray-300' : 'no-underline'}`}>
-                    Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-
-                  <button className="p-1 text-gray-300 rounded hover:bg-gray-400 hover:text-danger transition-colors">
-                    <Trash2 size={24}/>
-                  </button>
-                </article>
-
-              </div>
+              <ItensTasks />
             </div>
           </div>
         </main>
